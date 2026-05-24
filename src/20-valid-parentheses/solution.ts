36f2f2fb498ad9dbebadccb918e66366763ab4a8
Nikita Tolstik <vibegame.official@gmail.com>
2026-05-24 22:00:33 +0300

    solved 20 valid parentheses

--- src/20-valid-parentheses/solution.ts
+++ src/20-valid-parentheses/solution.ts
@@ -1,7 +1,29 @@
 // https://leetcode.com/problems/valid-parentheses/
 
 export function isValid(s: string): boolean {
-  void s;
+  const stack: string[] = [];
 
-  throw new Error("Not implemented");
+  const mirror = {
+    "(": ")",
+    "[": "]",
+    "{": "}",
+  };
+
+  const openBrackets = Object.keys(mirror);
+
+  for (let i = 0; i < s.length; i++) {
+    const char = s[i];
+
+    if (openBrackets.includes(char)) {
+      stack.push(mirror[char as keyof typeof mirror]);
+    } else {
+      const last = stack.pop();
+
+      if (last !== char) {
+        return false;
+      }
+    }
+  }
+
+  return !stack.length;
 }
